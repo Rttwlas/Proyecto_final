c34514a61c06bb644ad7e22ad831e04fb5fa8eac
Maximiliano Quiroga <107520203+Rttwlas@users.noreply.github.com>
2022-07-05 21:35:28 -0300

    Delete ProyectoFinal directory

--- ProyectoFinal/AppBazar/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-#---------------------------------------------------------------------------------------------------
-
-class BazarFormulario(forms.Form):
-    nombre= forms.CharField(max_length=50)
-    foto= forms.CharField(max_length=9999999)
-    precio= forms.IntegerField()
-    
